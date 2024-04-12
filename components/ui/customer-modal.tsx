5972c90e832d637d8e3faa4e10c3cc4d00803d05
BhimPrasadAdhikari <134574549+BhimPrasadAdhikari@users.noreply.github.com>
2024-04-12 15:28:17 +0545

    Update customer-modal.tsx

--- components/ui/customer-modal.tsx
+++ components/ui/customer-modal.tsx
@@ -1,23 +1,16 @@
 "use client";
 
-import usePreviewModal from "@/hooks/use-preview-modal";
-import Gallery from "@/components/gallery";
-import Info from "@/components/info";
+import useShipingForm from "@/hooks/useShipingForm";
 import Modal from "@/components/ui/modal";
 
 
-const PreviewModal = () => {
-  const previewModal = usePreviewModal();
-  const product = usePreviewModal((state) => state.data);
-
-  if (!product) {
-    return null;
-  }
+const ShippingModal = () => {
+  const shipingModal = useShippingForm();
 
   return ( 
     <Modal 
-      open={previewModal.isOpen} 
-      onClose={previewModal.onClose}
+      open={shipingModal.isOpen} 
+      onClose={shipingModal.onClose}
     >
       <div className="grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
         <div className="sm:col-span-4 lg:col-span-5">
