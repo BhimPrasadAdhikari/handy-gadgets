8fccd17d14962869968df7477104d7957a2ce52e
Bhim Prasad Adhikari <bhimprasadadhikari98@gmail.com>
2024-04-12 09:07:01 +0545

    final

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    
+        images:{
+domains:[
+    "res.cloudinary.com",
+    "https:/khalti.com"
+]
+        }
+    
+};
 
 export default nextConfig;
