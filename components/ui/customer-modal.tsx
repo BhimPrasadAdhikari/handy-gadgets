304f539f7e0bcc1f819b6b60c76790bd81110646
BhimPrasadAdhikari <134574549+BhimPrasadAdhikari@users.noreply.github.com>
2024-04-12 15:49:59 +0545

    Update customer-modal.tsx

--- components/ui/customer-modal.tsx
+++ components/ui/customer-modal.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import {ShippingForm} from "@/components/customer-detail“;
 import useShipingForm from "@/hooks/useShipingForm";
 import Modal from "@/components/ui/modal";
 
@@ -12,14 +12,7 @@ const ShippingModal = () => {
       open={shipingModal.isOpen} 
       onClose={shipingModal.onClose}
     >
-      <div className="grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
-        <div className="sm:col-span-4 lg:col-span-5">
-          <Gallery images={product.images} />
-        </div>
-        <div className="sm:col-span-8 lg:col-span-7">
-          <Info data={product} />
-        </div>
-      </div>
+      <ShippingForm />
     </Modal>
   );
 }
