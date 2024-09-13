816682e0dac91279daf4d24ea2bcd9d777b50acf
BhimPrasadAdhikari <bhimprasadadhikari98@gmail.com>
2024-09-13 09:30:52 +0545

    env

--- components/shiping-form.tsx
+++ components/shiping-form.tsx
@@ -79,12 +79,12 @@ export const ShipingForm: React.FC = () => {
 )
     setOpen(true);
     const details=response.data.details
-    console.log(details)
+    console.log(process.env.NEXT_PUBLIC_TEST_SECRET_KEY)
     const totalPrice=details?.reduce((total:any,product:any)=>{ return total+product.total_price},0)
     const Khalti_response = await axios.post('https://a.khalti.com/api/v2/epayment/initiate/',
       JSON.stringify({
-          "return_url": "http://localhost:3000/cart/",
-          "website_url": "http://localhost:3000/",
+          "return_url": `${process.env.NEXT_PUBLIC_STORE_PUBLIC_URL}cart/`,
+          "website_url": `${process.env.NEXT_PUBLIC_STORE_PUBLIC_URL}`,
           "amount": totalPrice+totalPrice*0.13,
           "purchase_order_id": "test12",
           "purchase_order_name": "test",
@@ -108,7 +108,7 @@ export const ShipingForm: React.FC = () => {
           "merchant_extra": "merchant_extra"
         }),
        { headers:{
-          "Authorization":"Key 1ac80a190b2749f994576e2d2e8f8f86 ",
+          "Authorization":`Key ${process.env.NEXT_PUBLIC_TEST_SECRET_KEY}`,
           "Content-Type":"application/json"
         }
 }
